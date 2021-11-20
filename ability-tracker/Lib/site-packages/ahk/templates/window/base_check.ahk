b53a3696be1f987b5b5b1834c95e0d27a3171f63
AlexanderLFloyd <91403167+LeMageTank@users.noreply.github.com>
2021-11-20 16:23:23 -0600

    Fixed problem where the venv wasn't used

--- ability-tracker/Lib/site-packages/ahk/templates/window/base_check.ahk
+++
@@ -1,7 +0,0 @@
-{% extends "base.ahk" %}
-{% block body %}
-if {{ command }}("{{ title }}")
-    FileAppend, 1, *
-else
-    FileAppend, 0, *
-{% endblock body %}
--- ability-tracker/Scripts/activate.bat
+++ ability-tracker/Scripts/activate.bat
@@ -8,7 +8,8 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\alexa\Desktop\lmt-rs3-ability-tracker\ability-tracker
+:: set VIRTUAL_ENV=C:\Users\alexa\Desktop\lmt-rs3-ability-tracker\ability-tracker
+set VIRTUAL_ENV="%~dp0\ability-tracker"
 
 if not defined PROMPT set PROMPT=$P$G
 
