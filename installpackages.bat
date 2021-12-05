fbcb960c54644fd52a11e294eb12b6ade7f0894c
AlexanderLFloyd <91403167+LeMageTank@users.noreply.github.com>
2021-12-04 19:01:14 -0600

    Removed unneeded dependency

--- installpackages.bat
+++ installpackages.bat
@@ -1,4 +1,4 @@
 @echo off
 set activatevenv="%~dp0ability-tracker\Scripts\activate.bat"
 set pip="%~dp0ability-tracker\Scripts\python.exe -m pip"
-echo %activatevenv% && pip install ahk && pip install pynput && pip install pyautogui && pip install pillow && pip install numpy
+echo %activatevenv% && pip install pynput && pip install pyautogui && pip install pillow && pip install numpy
--- tracker.bat
+++ tracker.bat
@@ -2,5 +2,5 @@
 set activatevenv="%~dp0ability-tracker\Scripts\activate.bat"
 set pythonpath="%~dp0ability-tracker\Scripts\python.exe"
 set mainscript="%~dp0ability-tracker\tracker\main.py"
-%activatevenv% && %pythonpath% %mainscript% & exit
+%activatevenv% && %pythonpath% %mainscript%
 
